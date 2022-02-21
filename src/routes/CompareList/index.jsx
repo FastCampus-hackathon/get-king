7b36e655ec2ec178f892588e1a8d7d922e78f264
kimyerimweb <kimyerimweb@gmail.com>
2022-02-22 02:00:01 +0900

    feat: Implement CompareList

--- src/routes/CompareList/index.jsx
+++ src/routes/CompareList/index.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import CompareItem from '../../components/CompareItem'
 
 function CompareList() {
-  return <div>비교 리스트 페이지</div>
+  const [list, setList] = useState()
+  useEffect(() => {
+    const { data } = axios.get('https://saramserver.herokuapp.com/set')
+    setList(data)
+  }, [])
+  return list.map((item) => <CompareItem item={item} />)
 }
 
 export default CompareList
