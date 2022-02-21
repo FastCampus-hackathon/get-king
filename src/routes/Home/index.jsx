49b19c256a4a11a8cc37567f72e4cbe2b8495d1b
kimyerimweb <kimyerimweb@gmail.com>
2022-02-21 23:56:36 +0900

    feat: Create Home

--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -1,7 +1,49 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
+import Detail from '../../components/Detail'
 
 function Home() {
-  return <div>안녕?</div>
+  const [text, setText] = useState('')
+  const [res, setRes] = useState()
+  const [res2, setRes2] = useState()
+  const onChange = (e) => {
+    setText(e.target.value)
+  }
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    const { data } = await axios.get(
+      'https://saramserver.herokuapp.com/saram/42385059'
+    )
+    setRes(data)
+  }
+
+  const onSubmit2 = async (e) => {
+    e.preventDefault()
+    const { data } = await axios.post(
+      'https://saramserver.herokuapp.com/saram',
+      { keywords: '패스트파이브', loc_cd: '101010' }
+    )
+    setRes2(data)
+  }
+
+  console.log(res)
+  console.log(res2)
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange} />
+        <button type="submit">검색</button>
+      </form>
+
+      <form onSubmit={onSubmit2}>
+        <input type="text" value={text} onChange={onChange} />
+        <button type="submit">검색2</button>
+      </form>
+      {res && <Detail data={res} />}
+    </>
+  )
 }
 
 export default Home
