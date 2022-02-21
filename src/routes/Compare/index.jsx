e63994dbb193c522ddb8649a9ec95b33c114cfaa
kimyerimweb <kimyerimweb@gmail.com>
2022-02-22 01:59:41 +0900

    feat: Implement Compare page

--- src/routes/Compare/index.jsx
+++ src/routes/Compare/index.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import Detail from '../../components/Detail'
 
 function Compare() {
-  return <div>비교페이지</div>
+  const employment1 = localStorage.getItem('employment1')
+  const employment2 = localStorage.getItem('employment2')
+  const employment3 = localStorage.getItem('employment3')
+
+  return (
+    <div>
+      <Detail data={employment1} />
+      <Detail data={employment2} />
+      <Detail data={employment3} />
+    </div>
+  )
 }
 
 export default Compare
