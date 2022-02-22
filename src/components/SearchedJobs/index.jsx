a64a347b1b8ad1fa5bef1137d22bc9a317e36fba
imhaeunk <imhaeunk@gmail.com>
2022-02-23 04:32:25 +0900

    [FIX]: 지역 없을시 예외처리

--- src/components/SearchedJobs/index.jsx
+++ src/components/SearchedJobs/index.jsx
@@ -4,8 +4,6 @@ import "dayjs/locale/ko";
 import { SearchedList } from "./style";
 import { useSelector } from "react-redux";
 import picked from "../../static/icons/picked.svg";
-import styled from "styled-components";
-import bell from "../../static/icons/bell.svg";
 import unpicked from "../../static/icons/unpicked.png";
 import { useDispatch } from "react-redux";
 import { addCompItem, deleteCompItem } from "../../reducers/comp";
@@ -13,117 +11,72 @@ import { addCompItem, deleteCompItem } from "../../reducers/comp";
 const SearchedJobs = () => {
   const [pickedList, setPickedList] = useState([]);
   const jobsData = useSelector(state => state.announcement.announcement);
-
+  console.log("잡데이터: ", jobsData);
   dayjs.locale("ko");
   const dispatch = useDispatch();
   const { compList } = useSelector(state => state.comp);
-
+  console.log(compList);
   return (
-    <>
-      <Container>
-        <div className="left">
-          <img src={bell} alt="scrap" />
-          <strong>스크랩 모아보기</strong>
-        </div>
+    <SearchedList>
+      {jobsData &&
+        jobsData.map((job, index) => {
+          const handleClick = job => {
+            if (compList.length === 3) {
+              alert("비교는 최대 3개까지 가능합니다.");
+              return;
+            }
+            const newPickedList = [...pickedList, job.id];
+            setPickedList(newPickedList);
 
-        <div className="right">
-          <select name="sort" id="sort">
-            <option value="date">마감일순</option>
-          </select>
-        </div>
-      </Container>
-      <SearchedList>
-        {jobsData &&
-          jobsData.map((job, index) => {
-            const handleClick = job => {
-              if (compList.length === 3) {
-                alert("비교는 최대 3개까지 가능합니다.");
-                return;
-              }
+            dispatch(addCompItem(job));
+            if (pickedList.includes(job.id)) {
+              const newPickedList = pickedList.filter(id => {
+                return id !== job.id;
+              });
+              setPickedList(newPickedList);
+              dispatch(deleteCompItem(job));
+            } else {
               const newPickedList = [...pickedList, job.id];
               setPickedList(newPickedList);
-
               dispatch(addCompItem(job));
-            };
-            const a = job.position.location.name.split(",");
-            // dispatchEvent(addItem(job))
-            return (
-              <li key={job.id}>
-                <div className="info_left">
-                  <h3 className="company_name">{job.company.detail.name}</h3>
-                  <div className="flex_wrapper">
-                    <h4 className="job_title">{job.position.title}</h4>
-                    <h4 className="job_salary">{job.salary.name}</h4>
-                    <h4 className="experience">
-                      {job.position["experience-level"].name}
-                    </h4>
-                  </div>
-                  <h4 className="job_tag">{job.position["job-code"].name}</h4>
+            }
+          };
+          // console.log(job?.position.location.name);
+          const a = job.position.location.name?.split(",");
+          return (
+            <li key={job.id}>
+              <div className="info_left">
+                <h3 className="company_name">{job.company.detail.name}</h3>
+                <div className="flex_wrapper">
+                  <h4 className="job_title">{job.position.title}</h4>
+                  <h4 className="job_salary">{job.salary.name}</h4>
+                  <h4 className="experience">
+                    {job.position["experience-level"].name}
+                  </h4>
                 </div>
-                <div className="info_right">
-                  <div>
-                    <h4 className="dead_line">
-                      {dayjs(
-                        new Date(job["expiration-timestamp"] * 1000)
-                      ).format("~MM/DD(dd)")}
-                    </h4>
-                    <img
-                      className="pick_btn"
-                      src={pickedList.includes(job.id) ? picked : unpicked}
-                      onClick={() => handleClick(job)}
-                      alt="icon"
-                    />
-                    <div className="loc">
-                      <h4>{job.position["job-type"].name}</h4>
-                      <h4>{a[a.length - 1].replace("&gt;", "/")}</h4>
-                    </div>
-                  </div>
+                <h4 className="job_tag">{job.position["job-code"].name}</h4>
+              </div>
+              <div className="info_right">
+                <div>
+                  <h4 className="dead_line">
+                    {dayjs(new Date(job["expiration-timestamp"] * 1000)).format(
+                      "~MM/DD(dd)"
+                    )}
+                  </h4>
+                  <img
+                    className="pick_btn"
+                    src={pickedList.includes(job.id) ? picked : unpicked}
+                    onClick={() => handleClick(job)}
+                    alt="icon"
+                  />
                 </div>
-              </li>
-            );
-          })}
-      </SearchedList>
-    </>
+                <h4>{job.position["job-type"].name}</h4>
+                <h4>{a ? a[a.length - 1].replace("&gt;", "/") : " "}</h4>
+              </div>
+            </li>
+          );
+        })}
+    </SearchedList>
   );
 };
-
 export default SearchedJobs;
-
-const Container = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 900px;
-  margin-left: 60px;
-  height: 28px;
-  margin-bottom: 16px;
-
-  .left {
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    width: 100%;
-    height: 100%;
-
-    img {
-      display: block;
-      margin-right: 4px;
-      align-self: center;
-      margin-bottom: 4px;
-    }
-
-    strong {
-      display: block;
-    }
-  }
-
-  .right {
-    select {
-      width: 80px;
-      height: 28px;
-      padding: 5px 0 5px 8px;
-      border: 1px solid ${({ theme }) => theme.colors.gray3};
-      border-radius: 4px;
-    }
-  }
-`;
