01843a67a1c838c7d43fd4808d428ae1b34089b0
imparang <jeongjaeyn@gmail.com>
2022-02-22 16:52:28 +0900

    로그인 & 글로벌 스타일

---
+++ src/styles/index.js
@@ -0,0 +1,58 @@
+const breakpoints = {
+  tablet: "1024px",
+  desktop: "1280px",
+};
+
+const fontFamilies = {
+  main: "font-family: 'Pretendard', sans-serif"
+};
+
+const fontSizes = {
+  title: "34px",
+  Subtitle: "24px",
+  Headline1: "22px",
+  Headline2: "22px",
+  Subheadline1: "20px",
+  Subheadline2: "20px",
+  body1: "16px",
+  body2: "16px",
+  body3: "16px",
+  footnote1: "14px",
+  footnote2: "14px",
+};
+
+const lineHeights = {
+  lineHeight: 1.3
+};
+
+const letterSpacings = {
+  letterSpacing: 0
+};
+
+const colors = {
+  primary: "#000000",
+  gray2: "#8A8A8A",
+  gray3: "#C4C4C4",
+  gray4: "#F7F7F7",
+
+  blue: "#4876EF",
+  lightBlue: "#B6C8F9",
+
+  green: "#00D3AB",
+  lightGreen: "#99EDDD",
+
+  red: "#EF4848",
+  lightRed: "#F9B6B6",
+
+  white: "#ffffff",
+};
+
+// styled-components에서 사용할 theme
+export const theme = {
+  breakpoints,
+  colors,
+  fontFamilies,
+  fontSizes,
+  lineHeights,
+  letterSpacings,
+};
