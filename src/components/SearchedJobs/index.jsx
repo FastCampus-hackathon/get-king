a9d9289aa56ec9a7bdf1ed7db396718e283ae6e2
imhaeunk <imhaeunk@gmail.com>
2022-02-22 23:25:26 +0900

    [STYLE]: 검색리스트 수정
    -

--- src/components/SearchedJobs/index.jsx
+++ src/components/SearchedJobs/index.jsx
@@ -10,29 +10,28 @@ const SearchedJobs = ({ jobsData }) => {
     <SearchedList>
       {jobsData &&
         jobsData.map((job, index) => {
+          const a = job.position.location.name.split(",")
           return (
             <li key={job.id}>
               <div className="info_left">
                 <h3 className="company_name">{job.company.detail.name}</h3>
                 <div className="flex_wrapper">
-                  <h4 className="job_tag">{job.position["job-code"].name}</h4>
-                  <h4>{job.salary.name}</h4>
-                  <h4>{job.position["experience-level"].name}</h4>
+                  <h4 className="job_title">{job.position.title}</h4>
+                  <h4 className="job_salary">{job.salary.name}</h4>
+                  <h4 className="experience">
+                    {job.position["experience-level"].name}
+                  </h4>
                 </div>
-                <h4>{job.position.title}</h4>
+                <h4 className="job_tag">{job.position["job-code"].name}</h4>
               </div>
-              <div className="info_left">
+              <div className="info_right">
                 <h4>
                   {dayjs(new Date(job["expiration-timestamp"] * 1000)).format(
                     "~MM/DD(dd)"
                   )}
                 </h4>
                 <h4>{job.position["job-type"].name}</h4>
-                <h4>
-                  {job.position.location.name
-                    .replace(/&lt;/g, "<")
-                    .replace(/&gt;/g, ">")}
-                </h4>
+                <h4>{a[a.length - 1].replace("&gt;", "/")}</h4>
               </div>
             </li>
           )
